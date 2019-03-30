f59021cf55c338f1ca33d854d01be94ad43f4851
fanthos <cby@cby.me>
2019-03-30 09:25:31 +0800

    Fix for chartjs 2.8.0

--- timeline.js
+++ timeline.js
@@ -16,6 +16,7 @@
         tooltips: {
             mode: 'nearest',
         },
+        adapters: {},
         time: {
             parser: false, // false == a pattern string from http://momentjs.com/docs/#/parsing/string-format/ or a custom callback that converts its argument to a moment
             format: false, // DEPRECATED false == date objects, moment object, callback or a pattern string from http://momentjs.com/docs/#/parsing/string-format/
